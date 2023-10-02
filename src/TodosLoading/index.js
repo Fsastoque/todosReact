8913c94f220fa716aac8f595d01df5d4cadbf07f
FabianSastoque <fabian.sastoque@panamericana.com.co>
2023-10-02 14:32:37 -0500

    Implementación loadingeskeletons en el elemento loading

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
-// import './TodosLoading.css'
+import './TodosLoading.css'
 
 function TodosLoading() {
   return (
-    <p>Cargando...</p>
+    // <p>Cargando...</p>
+    <div className='LoadignTodo-container skeleton'>
+      <span className='LoadingTodo-completed skeleton'></span>
+      <p className='LoadingTodo-text skeleton'></p>
+      <span className='LoadingTodo-icon-delete skeleton'></span>
+    </div>
   )
 }
 
